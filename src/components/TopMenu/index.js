9795ac8ddecc116befc3526eb528f882e84da4d2
brntava <bruno_tava7@hotmail.com>
2024-04-30 23:10:42 -0300

    refactor: refactored topMenu component

--- src/components/TopMenu/index.js
+++ src/components/TopMenu/index.js
@@ -1,76 +1,28 @@
-import { useState } from 'react'
-import { View, Text, Image, TouchableOpacity, Modal, Button } from 'react-native'
+import React, { useState } from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { ModalMenu } from '../ModalMenu/index';
 
 import { styles } from './styles';
 
 export const TopMenu = () =>{
-  const [modalVisible, setModalVisible] = useState(false)
+    const [modalVisible, setModalVisible] = useState(false);
 
-  return(
-    <View style={styles.container}>
-      <View style={styles.menuLogoContainer}>
-        <Image
-          style={styles.menuLogoImage}
-          source={require('../../../assets/paw-pet-login.png')}
-        />
-      </View>
-      <Text style={styles.menuTitle}>Lost Pets</Text>
-      <TouchableOpacity style={styles.menuConfigContainer} onPress={() => setModalVisible(true)}>
-        <Image
-          style={styles.menuBarImage}
-          source={require('../../../assets/menu-lines.png')}
-        />
-      </TouchableOpacity >
-
-      <Modal
-        animationType="fade"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => setModalVisible(false)}
-      >
-        <View style={styles.modalCloseButtonContainer}>
-          <TouchableOpacity
-            onPress={() => setModalVisible(false)} 
-          >
-            <Image
-              style={styles.modalCloseButton}
-              source={require('../../../assets/close-icon.png')}
-            />
-          </TouchableOpacity>
+    return(
+        <View style={styles.container}>
+            <View style={styles.menuLogoContainer}>
+                <Image
+                    style={styles.menuLogoImage}
+                    source={require('../../../assets/paw-pet-login.png')}
+                />
+            </View>
+            <Text style={styles.menuTitle}>Lost Pets</Text>
+            <TouchableOpacity style={styles.menuConfigContainer} onPress={() => setModalVisible(true)}>
+                <Image
+                    style={styles.menuBarImage}
+                    source={require('../../../assets/menu-lines.png')}
+                />
+            </TouchableOpacity >
+            {modalVisible && <ModalMenu visible={modalVisible} closeModal={() => setModalVisible(false)} />}
         </View>
-        <View style={styles.modalContainer}>
-          <Text style={styles.modalTitle}>
-            Menu
-          </Text>
-          
-          <TouchableOpacity
-            style={styles.modalOption}
-            title='Perfil'
-          >
-            <Text>
-              Perfil
-            </Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={styles.modalOption}
-            title='Configurações'
-          >
-            <Text>
-              Configurações
-            </Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={[styles.modalOption, styles.modalOptionLogout]}
-            title='Sair'
-          >
-            <Text>
-              Sair
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </Modal>
-    </View>
-  )
-}
+    );
+};
